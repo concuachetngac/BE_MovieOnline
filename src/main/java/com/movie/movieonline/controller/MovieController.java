d2415764d48ad0784b1f57866120aa9c6aee05d4
concuachetngac <ductuantran2410@gmail.com>
2022-12-12 15:58:23 +0700

    NEwwwww

--- src/main/java/com/movie/movieonline/controller/MovieController.java
+++ src/main/java/com/movie/movieonline/controller/MovieController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.movie.movieonline.domain.Comment;
 import com.movie.movieonline.domain.Genre;
 import com.movie.movieonline.domain.Movie;
 import com.movie.movieonline.service.GenreService;
@@ -83,13 +84,18 @@ public class MovieController {
     @GetMapping("all")
     public List<Movie> getAllMovies(){
         List<Movie> myList =movieService.getAllMovies();
-        Collections.sort(myList, new Comparator<Movie>() {
-            public int compare(Movie o1, Movie o2) {
-                if (o1.getRelease_date() == null || o2.getRelease_date() == null)
-                  return 0;
-                return o2.getRelease_date().compareTo(o1.getRelease_date());
-            }
-          });
+        
+        for(Movie m:myList){
+            List<Comment> listComment = m.getComments();
+            Collections.sort(listComment, new Comparator<Comment>() {
+                public int compare(Comment o1, Comment o2) {
+                    if (o1.getTime() == null || o2.getTime() == null)
+                      return 0;
+                    return o2.getTime().compareTo(o1.getTime());
+                }
+              });
+              m.setComments(listComment);
+        }
         return myList;
     }
 
@@ -103,6 +109,15 @@ public class MovieController {
     @GetMapping("{id}")
     public ResponseEntity<Movie> getMovieById(@PathVariable long id){
         Movie movie = movieService.getMovieById(id);
+        List<Comment> listComment = movie.getComments();
+        Collections.sort(listComment, new Comparator<Comment>() {
+            public int compare(Comment o1, Comment o2) {
+                if (o1.getTime() == null || o2.getTime() == null)
+                  return 0;
+                return o2.getTime().compareTo(o1.getTime());
+            }
+          });
+        movie.setComments(listComment);
         return new ResponseEntity<Movie>(movie, HttpStatus.OK);
     }
 }
